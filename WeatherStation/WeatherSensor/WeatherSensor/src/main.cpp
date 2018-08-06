600f6dc163d3d3d5c3121f55e791cd5cf09f661b
ilyadodge <ilya-dodge@mail.ru>
2018-08-06 21:44:48 +0700

    1

--- WeatherStation/WeatherSensor/WeatherSensor/src/main.cpp
+++ WeatherStation/WeatherSensor/WeatherSensor/src/main.cpp
@@ -1,9 +1,94 @@
-#include <Arduino.h>
+/*   Данный скетч делает следующее: передатчик (TX) отправляет массив
+ *   данных, который генерируется согласно показаниям с кнопки и с 
+ *   двух потенциомтеров. Приёмник (RX) получает массив, и записывает
+ *   данные на реле, сервомашинку и генерирует ШИМ сигнал на транзистор.
+    by AlexGyver 2016
+*/
+
+#include <SPI.h>          // библиотека для работы с шиной SPI
+#include "nRF24L01.h"     // библиотека радиомодуля
+#include "RF24.h"         // ещё библиотека радиомодуля
+#include <OneWire.h>
+
+OneWire  ds(3);  // on pin 10 (a 4.7K resistor is necessary)
+RF24 radio(9, 10); // "создать" модуль на пинах 9 и 10 Для Уно
+//RF24 radio(9,53); // для Меги
+
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+
+byte counter;
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600); //открываем порт для связи с ПК
+
+  radio.begin(); //активировать модуль
+  radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);     //размер пакета, в байтах
+
+  radio.openWritingPipe(address[0]);   //мы - труба 0, открываем канал для передачи данных
+  radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
+
+  radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate (RF24_250KBPS); //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+  //должна быть одинакова на приёмнике и передатчике!
+  //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+
+  radio.powerUp(); //начать работу
+  radio.stopListening();  //не слушаем радиоэфир, мы передатчик
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop(void) {
+ ////////////////////////////////////////sensor//////////////////////////
+ 
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius;
+  
+  if ( !ds.search(addr)) {  
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44);        
+  delay(1000);       
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE); 
+         
+for ( i = 0; i < 9; i++) {           
+  data[i] = ds.read();
+ (data[i], HEX);  
+ }
+
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; 
+    if (data[7] == 0x10) {
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw & ~7; 
+    else if (cfg == 0x20) raw = raw & ~3;
+    else if (cfg == 0x40) raw = raw & ~1; 
+  }
+  celsius = (float)raw / 16.0;
+  Serial.print(celsius);
+  //Serial.print("Sent: "); Serial.println(celsius);
+  radio.write(&celsius, float(celsius));
+////////////////////////////////////////sensor end //////////////////////////
+ 
+  
 }
