17a8fbfa200ae09875d76c143b9f52319d6c9fc9
ilyadodge <ilya-dodge@mail.ru>
2018-08-08 21:17:25 +0700

    передача показаний с одного датчика, постоянная работа радио модуля, без проверок на новые показания, без экономии энергии

--- WeatherStation/WeatherSensor/WeatherSensor/src/main.cpp
+++ WeatherStation/WeatherSensor/WeatherSensor/src/main.cpp
@@ -13,30 +13,21 @@
 OneWire  ds(3);  // on pin 3 (a 4.7K resistor is necessary)
 RF24 radio(9, 10); // "создать" модуль на пинах 9 и 10 Для Уно
 //RF24 radio(9,53); // для Меги
-
-
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
-
-
-
 
 void setup() {
   Serial.begin(9600); //открываем порт для связи с ПК
-
   radio.begin(); //активировать модуль
   radio.setAutoAck(1);         //режим подтверждения приёма, 1 вкл 0 выкл
   radio.setRetries(0, 15);    //(время между попыткой достучаться, число попыток)
   radio.enableAckPayload();    //разрешить отсылку данных в ответ на входящий сигнал
   radio.setPayloadSize(32);     //размер пакета, в байтах
-
   radio.openWritingPipe(address[0]);   //мы - труба 0, открываем канал для передачи данных
   radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
-
   radio.setPALevel (RF24_PA_MAX); //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
   radio.setDataRate (RF24_250KBPS); //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
   //должна быть одинакова на приёмнике и передатчике!
   //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
-
   radio.powerUp(); //начать работу
   radio.stopListening();  //не слушаем радиоэфир, мы передатчик
 }
@@ -62,8 +53,7 @@ void loop(void) {
   Serial.println();
   ds.reset();
   ds.select(addr);
-  ds.write(0x44);        
-  //delay(1000);       
+  ds.write(0x44);             
   present = ds.reset();
   ds.select(addr);    
   ds.write(0xBE); 
@@ -87,7 +77,7 @@ for ( i = 0; i < 9; i++) {
   }
   float data1[2];
   data1[0] = float(raw) / 16.0;
-  Serial.print("Sent1: "); Serial.println(float(raw) / 16.0);
+ // Serial.print("Sent1: "); Serial.println(float(raw) / 16.0);
   radio.write(&data1, sizeof(data1));
   delay(500);
 }
